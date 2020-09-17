5209dbbb6748c5f3f94c88172834eea82f6ef707
julien terrier <julien.tr99@gmail.com>
2020-09-17 03:15:02 +0200

    adding project

---
+++ index.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon:'C:\Users\julie\Documents\Programmation\Electron app\logo.png',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
